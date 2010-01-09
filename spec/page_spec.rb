01799bcb80b72e1be765c03e62384b7564f50b0a
remi <remi@remitaylor.com>
2010-01-09 15:40:22 -0700

    updated create_page so it's easy to create the body of pages from specs

--- spec/page_spec.rb
+++ spec/page_spec.rb
@@ -4,6 +4,18 @@ require 'fileutils'
 
 # Helper for creating page files in the specs, at spec/pages/
 def create_page filename, body = ''
+  body.sub!("\n", '')     if body.lines.first == "\n" # get rid of first empty line
+  body.sub!(/\n\s+$/, '') if body.lines.count > 0     # get rid of last  empty line
+
+  # grab the spaces (if any) from the 1st line and remove that
+  # many spaces from the rest of the lines so our specs look clean
+  if body.lines.first
+    puts body.lines.first.inspect
+    spaces = body.lines.first[/^[ ]+/]
+    puts "SPACES: #{ spaces.inspect }"
+    body.gsub!(/^#{spaces}/, '') if spaces
+  end
+
   FileUtils.mkdir_p Page.dir
   File.open(File.join(Page.dir, filename), 'w'){|f| f << body }
 end
@@ -20,6 +32,24 @@ describe Page do
   before do
     Page.dir = File.dirname(__FILE__) + '/pages'
     delete_pages
+  end
+
+  it '#create_page works as expected for creating fake pages for these specs' do
+    path = File.dirname(__FILE__) + '/pages/foo.markdown'
+    File.file?(path).should be_false
+
+    create_page 'foo.markdown'
+
+    File.file?(path).should be_true
+    Page[:foo].body.should == ''
+
+    create_page 'foo.markdown', %{
+      it should fix
+        the spaces
+      for me
+    }
+
+    Page[:foo].body.should == "it should fix\n  the spaces\nfor me"
   end
 
   it 'can set the Page.dir to specify the directory to fetch pages from'
@@ -76,7 +106,18 @@ describe Page do
     Page[:foo].should_not     be_nil
   end
 
-  it 'can add any kind of arbitrary data to a page via YAML'
+  it 'can add any kind of arbitrary data to a page via YAML' do
+    pending
+    create_page 'foo.markdown', %{
+      ---
+      title:  RubyFlow is protecting you from great articles!
+      layout: post
+      date:   2009-04-06
+      tags:   rubyflow, ruby, news
+      ---
+      Hello World!
+    }
+  end
 
   it 'can get a page by the value of any arbitrary data (from the YAML)'
 
