a33a7516206db983fd5d63dfc78c631aa87f3d66
remi <remi@remitaylor.com>
2010-01-10 15:21:51 -0700

    shared example refactoring

--- lib/jadof/spec/helpers.rb
+++ lib/jadof/spec/helpers.rb
@@ -1,4 +1,15 @@
 # Helper for creating page files in the specs, at spec/pages/
+#
+#   create_page 'foo.markdown'
+#
+#   create_page 'foo.markdown', %{
+#     This is the body
+#     of the page that will
+#     be created.
+#   }
+#
+#   create_page 'foo.markdown', 'body',  './directory/to/create/page/in/'
+#
 def create_page filename, body = '', page_dir = JADOF::Page.dir
   body.sub!("\n", '')     if body.lines.first == "\n" # get rid of first empty line
   body.sub!(/\n\s+$/, '') if body.lines.count > 0     # get rid of last  empty line
